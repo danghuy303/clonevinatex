9a8ad5816aa67ea59b122129e914eff77e9e2b9d
Ghost.Vn <lequocdat113@gmail.com>
2021-01-14 15:54:43 +0700

    commit lay code Vu

--- src/app/quantri/quanlykhosanxuat/phuongan/layoutmodals/dongvanpx1/dongvanpx1.component.ts
+++ src/app/quantri/quanlykhosanxuat/phuongan/layoutmodals/dongvanpx1/dongvanpx1.component.ts
@@ -10,10 +10,25 @@ import { SanXuatService } from 'src/app/services/callApiSanXuat';
 })
 export class Dongvanpx1Component implements OnInit {
   checkbutton:any={};
+  listLoBong:any=[];
+  item:any = {};
+  block1:any = [];
+  block2:any = [];
+  block3:any = [];
+  block4:any = [];
+  poolLoBong:any = [];
+  banBong:any = {};
   constructor(public _activeModal: NgbActiveModal, private _services: SanXuatService, public _toastr: ToastrService, public _modal: NgbModal) {
   }
 
   ngOnInit(): void {
+    for(let i=1; i<=50;i++){
+      this.banBong[`${i}`]={
+        _focus:false,
+        labelLoBong:''
+      }
+    };
+    console.log(this.block1);
   }
 
 }
