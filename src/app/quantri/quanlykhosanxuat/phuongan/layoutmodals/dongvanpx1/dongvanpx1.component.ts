c7021f6c02332dd63237518d47453f11a8723da8
Ghost.Vn <lequocdat113@gmail.com>
2021-01-14 16:36:07 +0700

    commit lay code Vu

--- src/app/quantri/quanlykhosanxuat/phuongan/layoutmodals/dongvanpx1/dongvanpx1.component.ts
+++ src/app/quantri/quanlykhosanxuat/phuongan/layoutmodals/dongvanpx1/dongvanpx1.component.ts
@@ -18,6 +18,7 @@ export class Dongvanpx1Component implements OnInit {
   block4:any = [];
   poolLoBong:any = [];
   banBong:any = {};
+  ngoaiQuan:any = [];
   constructor(public _activeModal: NgbActiveModal, private _services: SanXuatService, public _toastr: ToastrService, public _modal: NgbModal) {
   }
 
@@ -25,10 +26,26 @@ export class Dongvanpx1Component implements OnInit {
     for(let i=1; i<=50;i++){
       this.banBong[`${i}`]={
         _focus:false,
-        labelLoBong:''
+        labelLoBong:'',
+        STT:`${i}. `
+      }
+      if(i<=2){
+        this.block1.push(`${i}`)
+      }
+      if(2<i && i<=16){
+        this.block2.push(`${i}`)
+      }
+      if(16<i &&i<=48){
+        this.block3.push(`${i}`)
+      }
+      if(48<i && i<=50){
+        this.block4.push(`${i}`)
       }
     };
-    console.log(this.block1);
+    console.log(this.block1)
+    console.log(this.block2)
+    console.log(this.block3)
+    console.log(this.block4)
   }
 
 }
