31536fbdca8a902723ba259feb9c31b51a8b2c92
unknown <nguyenvu5994@gmail.com>
2021-01-19 17:20:43 +0700

    dinh muc tieu chi

--- src/app/quantri/danhmuc/danhmucsanxuat/dinhmuctieuchichatluongsoi/dinhmuctieuchichatluongsoi.component.ts
+++ src/app/quantri/danhmuc/danhmucsanxuat/dinhmuctieuchichatluongsoi/dinhmuctieuchichatluongsoi.component.ts
@@ -17,128 +17,79 @@ export class DinhmuctieuchichatluongsoiComponent implements OnInit {
   items: any = [
   ];
   paging: any = { CurrentPage: 1, TotalPage: 1, TotalItem: 0 };
-  keyWord:any='';
-  filter:any={
+  keyWord: any = '';
+  filter: any = {
   };
-  cols: any = [   
+  cols: any = [
     {
       header: 'Mã mặt hàng',
       field: 'Ma',
-      width: '100px',
-      align:'center'
+      width: 'unset',
+      align: 'center'
     },
     {
       header: 'Tên mặt hàng',
       field: 'Ten',
-      width: '200px',
-      align:'center'
-    },    
+      width: 'unset',
+      align: 'center'
+    },
     {
       header: 'Ghi chú',
       field: 'GhiChu',
       width: 'unset',
-      align:'left'
+      align: 'left'
     }
   ];
   selectedItems: any = [];
-  constructor(private _modal:NgbModal,
-    private _services:SanXuatService,
-    private _toastr:ToastrService) 
-    { }
+  constructor(private _modal: NgbModal,
+    private _services: SanXuatService,
+    private _toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.GetListdm();
   }
-  resetFilter(){
+  resetFilter() {
     this.filter = {
     };
     this.GetListdm()
   }
- 
-  GetListdm(reset?){
-    if(reset){
-      this.paging.CurrentPage=1;
+
+  GetListdm(reset?) {
+    if (reset) {
+      this.paging.CurrentPage = 1;
       this.paginator.changePage(0);
     }
     let data = {
-      PageSize:20, 
-      CurrentPage:this.paging.CurrentPage,
-      sFilter:this.filter.keyWord?this.filter.keyWord:'',
-      CongDoan:this.filter.CongDoan?this.filter.CongDoan:'',
-      Ma:"", 
-      Ten:"",
+      PageSize: 20,
+      CurrentPage: this.paging.CurrentPage,
+      sFilter: this.filter.keyWord ? this.filter.keyWord : '',
+      IddmLoaiSoi: this.filter.IddmLoaiSoi ? this.filter.IddmLoaiSoi : '',
+      Ma: "",
+      Ten: "",
+      Loai: "1",
     };
-    this._services.GetListDinhMuc(data).subscribe((res:any)=>{
+    this._services.GetListdmItem(data).subscribe((res: any) => {
       this.items = res.items;
       this.paging = res.paging;
     })
   }
-  add(){
-    let modalRef = this._modal.open(DinhmuctieuchichatluongsoimodalComponent,{
-      backdrop:'static',
-    });
-    modalRef.componentInstance.opt='add';
 
-    modalRef.result.then(res=>{
-      this.GetListdm()
-    }).catch(er=>console.log(er))
-  }
-  edit(item){
-    this._services.GetDinhMuc(item.Id).subscribe((res:any)=>{
-    let modalRef = this._modal.open(DinhmuctieuchichatluongsoimodalComponent,{
-      backdrop:'static',
-    });
-    modalRef.componentInstance.opt='edit';
-    modalRef.componentInstance.item = JSON.parse(JSON.stringify(res));
-    modalRef.result.then(res=>{
-      this.GetListdm()
-    }).catch(er=>console.log(er))
+  edit(item) {
+    this._services.dmDinhMucTieuChiChatLuongSoi().Get({ idSanPham: item.Id }).subscribe((res: any) => {
+      let modalRef = this._modal.open(DinhmuctieuchichatluongsoimodalComponent, {
+        backdrop: 'static',
+      });
+      modalRef.componentInstance.opt = 'edit';
+      modalRef.componentInstance.item = res;
+      modalRef.componentInstance.SelectItem = item;
+      modalRef.result.then(res => {
+        this.GetListdm()
+      }).catch(er => console.log(er))
     })
   }
 
-  delete(item){
-    let modalRef = this._modal.open(ModalthongbaoComponent,{
-      backdrop:'static'
-    });
-    let itemDel : any = {};
-    itemDel.Id = item.Id;
-    modalRef.componentInstance.message='Bạn có chắc chắn muốn xóa dữ liệu vừa chọn?';
-    modalRef.result.then(res=>{
-
-      this._services.DeleteDinhMuc(itemDel).subscribe((res: any) => {
-        if (res) {
-          if (res.State === 1) {
-            this._toastr.success(res.message);
-            this.GetListdm();
-          } else {
-            this._toastr.error(res.message);
-          }
-        }
-      })
-    }).catch(er=>console.log(er))
-  }
-  deleteAll(){
-    let modalRef = this._modal.open(ModalthongbaoComponent,{
-      backdrop:'static',
-      size: 'fullscreen',
-    });
-    modalRef.componentInstance.message='Bạn có chắc chắn muốn xóa dữ liệu vừa chọn?';
-    modalRef.result.then(res=>{
-      this._services.DeleteDinhMuc(this.selectedItems).subscribe((res: any) => {
-        if (res) {
-          if (res.State === 1) {
-            this._toastr.success(res.message);
-            this.GetListdm();
-            this.selectedItems = [];
-          } else {
-            this._toastr.error(res.message);
-          }
-        }
-      })
-    }).catch(er=>console.log(er))
-  }
-  changePage(event){
-    this.paging.CurrentPage = event.page+1;
+  changePage(event) {
+    this.paging.CurrentPage = event.page + 1;
     this.GetListdm();
   }
 
