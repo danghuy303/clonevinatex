8809b94d066414a25ed8354452c833898e1ad944
An Thị Hương <anhuong97@gmail.com>
2021-04-28 11:01:43 +0700

    phan quyen

--- src/app/quantri/quantri.component.ts
+++ src/app/quantri/quantri.component.ts
@@ -525,6 +525,7 @@ export class QuantriComponent implements OnInit {
                     {
                         label: 'Phiếu điều chỉnh',
                         routerLink: '/quantri/trienkhaisanxuat/phieudieuchinh/0',
+                        separator: this.checkmenu("DIEUCHINHDOIKIENBONG"),
                         command: () => this.close()
                     },
                     {
