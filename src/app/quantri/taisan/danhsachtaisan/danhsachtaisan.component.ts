cd4f5c1516d4b780b71e65ffc8c339143bdf0853
nguyenvu59 <nguyenvu5994@gmail.com>
2021-10-13 10:57:04 +0700

    services tai san

--- src/app/quantri/taisan/danhsachtaisan/danhsachtaisan.component.ts
+++ src/app/quantri/taisan/danhsachtaisan/danhsachtaisan.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { number } from "@amcharts/amcharts4/core";
+import { HopDongService } from "src/app/services/Hopdong/hopdong.service";
+
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { ToastrService } from "ngx-toastr";
+import { SanXuatService } from "src/app/services/callApiSanXuat";
+
+import {
+  DateToUnix,
+  mapArrayForDropDown,
+  UnixToDate,
+} from "src/app/services/globalfunction";
 
 @Component({
   selector: 'app-danhsachtaisan',
