6edc80ea24faf366d7f1870079d700d2f0abd7df
nguyenvu59 <nguyenvu5994@gmail.com>
2021-10-19 14:14:56 +0700

    nhap tai san

--- src/app/quantri/taisan/modal/modalcapnhattaisancon/modalcapnhattaisancon.component.ts
+++ src/app/quantri/taisan/modal/modalcapnhattaisancon/modalcapnhattaisancon.component.ts
@@ -4,6 +4,7 @@ import { FileUploader } from 'ng2-file-upload';
 import { ToastrService } from 'ngx-toastr';
 import { SanXuatService } from 'src/app/services/callApiSanXuat';
 import { vn } from 'src/app/services/const';
+import { mapArrayForDropDown, validVariable, DateToUnix, DateToDatePicker, UnixToDate, deepCopy } from 'src/app/services/globalfunction';
 import { DanhmuctaisanService } from 'src/app/services/Taisan/danhmuctaisan.service';
 import { TaisanService } from 'src/app/services/Taisan/taisan.service';
 
@@ -14,6 +15,7 @@ import { TaisanService } from 'src/app/services/Taisan/taisan.service';
 })
 export class ModalcapnhattaisanconComponent implements OnInit {
 
+  item: any = {};
   opt: any = "";
   title: any = "";
   lang: any = vn;
@@ -23,6 +25,9 @@ export class ModalcapnhattaisanconComponent implements OnInit {
   listLoaiTaiSan: any = [];
   listTinhTrangTaiSan: any = [];
   listNhaSanXuat: any = [];
+  listTaiSan: any = [];
+  listTinhTrangTaiSan_copy: any = [];
+  listDonVi_copy: any = [];
 
   constructor(
     public _modal: NgbModal,
@@ -37,14 +42,42 @@ export class ModalcapnhattaisanconComponent implements OnInit {
   ngOnInit(): void {
     if (this.opt === 'add') {
       this.title = "Thêm mới tài sản con";
+      this.item = {
+        Id: "",
+        isXoa: false,
+        listFileDinhKem: [],
+        CreatedBy: new Date(),
+        ModifiedBy: new Date(),
+      }
     }
     else {
       this.title = "Cập nhật tài sản con";
     }
   }
 
-  GhiLai() {
-
+  Validate() {
+    if (!validVariable(this.item.Ma) ||
+      !validVariable(this.item.Ten) ||
+      !validVariable(this.item.NgayNhap) ||
+      !validVariable(this.item.ThoiGianSuDung) ||
+      !validVariable(this.item.SoSeri) ||
+      !validVariable(this.item.IddmTinhTrang)) {
+      this.toastr.error("Yêu cầu nhập đầy đủ trường bắt buộc");
+      return false;
+    }
+    return true;
   }
 
+  GhiLai() {
+    if (this.Validate()) {
+      this.item.NgaySanXuatUnix = DateToUnix(this.item.NgaySanXuat);
+      this.item.NgayNhapUnix = DateToUnix(this.item.NgayNhap);
+      this.item.MaTinhTrang = this.listTinhTrangTaiSan_copy.find(obj => obj.Id === this.item.IddmTinhTrang).Ma;
+      if (validVariable(this.item.IddmDonViTinh)) {
+        this.item.TenDonViTinh = this.listDonVi_copy.find(obj => obj.Id === this.item.IddmDonViTinh).Ten;
+      }
+      this.listTaiSan.push(this.item);
+      this.activeModal.close();
+    }
+  }
 }
