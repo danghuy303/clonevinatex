5caaa5a49af9a6b5a60e3b6a2f2a9abfbb6f31ec
Ghost.Vn <lequocdat113@gmail.com>
2021-10-29 17:42:56 +0700

    add modal tinh doanh thu

--- src/app/quantri/hopdong/modals/tinhdoanhthumodal/tinhdoanhthumodal.component.ts
+++ src/app/quantri/hopdong/modals/tinhdoanhthumodal/tinhdoanhthumodal.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { AuthenticationService } from 'src/app/services/auth.service';
 import { SanXuatService } from 'src/app/services/callApiSanXuat';
 import { DanhMucHopDongService } from 'src/app/services/Hopdong/danhmuchopdong.service';
 import { StoreService } from 'src/app/services/store.service';
+import { PintableDirective } from 'voi-lib';
 
 @Component({
   selector: 'app-tinhdoanhthumodal',
@@ -14,6 +15,7 @@ import { StoreService } from 'src/app/services/store.service';
 export class TinhdoanhthumodalComponent implements OnInit {
   item: any = {};
   checkbutton:any={};
+  @ViewChild(PintableDirective) voiPintable: PintableDirective;
   constructor(public activeModal: NgbActiveModal, private _danhMucHopDong: DanhMucHopDongService,
     public toastr: ToastrService,
     private _services: SanXuatService,
