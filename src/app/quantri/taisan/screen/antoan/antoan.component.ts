15455efb5a67319cfb3951bcb55290ac718ada4d
nguyenvu59 <nguyenvu5994@gmail.com>
2021-11-12 09:09:19 +0700

    cap nhat tai san

---
+++ src/app/quantri/taisan/screen/antoan/antoan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-antoan',
+  templateUrl: './antoan.component.html',
+  styleUrls: ['./antoan.component.css']
+})
+export class AntoanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
