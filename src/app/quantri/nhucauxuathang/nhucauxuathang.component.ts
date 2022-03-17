1759407d9e5c9ea3a35b0aa110b1a0fb4d98f1ff
lequocdat113 <lequocdat113@gmail.com>
2022-03-17 11:15:21 +0700

    fix button xuat bao cao

--- src/app/quantri/nhucauxuathang/nhucauxuathang.component.ts
+++ src/app/quantri/nhucauxuathang/nhucauxuathang.component.ts
@@ -115,7 +115,8 @@ export class NhucauxuathangComponent implements OnInit, OnDestroy {
       }
     },
     legend: {
-      position: 'left'
+      position: 'left',
+      display:false
     },
     tooltips: {
       callbacks: {
