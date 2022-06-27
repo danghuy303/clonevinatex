549f0bce92209b63edc89e5e48abb1a32a44234e
huync1105 <huync1105@gmail.com>
2022-06-27 15:15:03 +0700

    Fix lich xich thang

--- src/app/quantri/taisan/screenLichXich/taisanlichxichthang/taisanlichxichthang.component.ts
+++ src/app/quantri/taisan/screenLichXich/taisanlichxichthang/taisanlichxichthang.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
+import { deepCopy } from 'src/app/services/globalfunction';
 import { TaisanService } from 'src/app/services/Taisan/taisan.service';
 import { PintableDirective } from 'voi-lib';
 import { ModalluachonbaoduonglichxichtheothangComponent } from '../../modal/modalluachonbaoduonglichxichtheothang/modalluachonbaoduonglichxichtheothang.component';
@@ -16,6 +17,7 @@ export class TaisanlichxichthangComponent implements OnInit, OnChanges, AfterVie
   @Input('checkBtnChonTaiSan') checkBtnChonTaiSan: boolean;
   @Input('soNgay') soNgay: any = 0;
   @Input('ThoiGianDaChon') thoiGianDaChon:any;
+  keyword: string;
   labelThang = [];
   selectedItems = [];
   copyItemsBaoDuong = [];
@@ -32,55 +34,21 @@ export class TaisanlichxichthangComponent implements OnInit, OnChanges, AfterVie
     for (let i = 1; i <= this.soNgay; i++) {
       this.labelThang.push(i);
     }
-    // console.log("listTaiSan", this.listTaiSan);
+    console.log("listTaiSan", this.listTaiSan);
   }
   ngAfterViewInit(): void {
-    this.voiPintable.active();
+    // this.voiPintable.active();
   }
 
   ngOnInit(): void {
 
   }
 
-  getBaoDuong(taisan, baoduong) {
-    // console.log("baoduong", baoduong);
-    // console.log("taisan",taisan);
-    
-    taisan.listbdDaChon = [];
-    taisan.listBaoDuong.forEach(obj => {
-      if (obj.listChiTiet?.length) {
-        obj.listChiTiet.forEach(ele => {
-          // taisan.listbdDaChon.push(ele.IddmLoaiBaoDuong);
-          // taisan.listbdDaChon.push(ele.Id);
-          taisan.listbdDaChon.push(ele.IdTaiSan_BaoDuong);
-        })
-      }
+  check(e, index, list) {
+    list.forEach(ele => {
+      ele.isChon = false;
     })
-    // console.log("taisan.listbdDaChon", taisan.listbdDaChon);
-    let modalRef = this._modal.open(ModalluachonbaoduonglichxichtheothangComponent, {
-      size: 'lg',
-      backdrop: 'static',
-    })
-    modalRef.componentInstance.taiSan = taisan;
-    modalRef.componentInstance.thoiGianDaChon = this.thoiGianDaChon;
-    modalRef.componentInstance.listItemDaChon = taisan.listbdDaChon || [];
-    // modalRef.componentInstance.listItemsTrongBd = baoduong.listChiTiet ?  baoduong.listChiTiet.map(ele => ele?.IddmLoaiBaoDuong) : [];
-    // modalRef.componentInstance.listItemsTrongBd = baoduong.listChiTiet ?  baoduong.listChiTiet.map(ele => ele?.Id) : [];
-    modalRef.componentInstance.checkBtnChonTaiSan = this.checkBtnChonTaiSan;
-    // modalRef.componentInstance.listItemsTrongBd = baoduong.listChiTiet ?  baoduong.listChiTiet.map(ele => ele?.Id) : [];
-    modalRef.componentInstance.listItemsTrongBd = baoduong.listChiTiet ?  baoduong.listChiTiet.map(ele => ele?.IdTaiSan_BaoDuong) : [];
-    modalRef.result
-      .then((res: any) => {
-        baoduong.listChiTiet = res;
-        // taisan.listBaoDuong.forEach(ele => {
-        //   ele?.listChiTiet.forEach(obj => {
-        //     // this.selectedItems.push(obj.IddmLoaiBaoDuong)
-        //     this.selectedItems.push(obj.Id)
-        //   })
-        // })
-      })
-      .catch(error => {
-      })
-      .finally()
+    list[index].isChon = e.checked;
   }
+
 }
