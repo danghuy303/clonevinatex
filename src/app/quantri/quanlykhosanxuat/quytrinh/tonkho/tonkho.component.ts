18abdccb0093b31bb3c8625e1fdd2abc4728e14f
khacduyet <duyetbuikhac@gmail.com>
2024-01-16 17:24:15 +0700

    upd

--- src/app/quantri/quanlykhosanxuat/quytrinh/tonkho/tonkho.component.ts
+++ src/app/quantri/quanlykhosanxuat/quytrinh/tonkho/tonkho.component.ts
@@ -13,12 +13,12 @@ import { TonkhodanhsachchitietComponent } from '../tonkhodanhsachchitiet/tonkhod
   templateUrl: './tonkho.component.html',
   styleUrls: ['./tonkho.component.css']
 })
-export class TonkhoComponent  extends StoreBase implements OnInit,OnDestroy {
+export class TonkhoComponent extends StoreBase implements OnInit, OnDestroy {
   @ViewChild('paginator') paginator: any;
-  items: any = [{id:5,SoQuyTrinh:'PNK_0000_0000'}];
-  filter:any={};
-  listdmKho:any=[];
-  trangThai:any=1;
+  items: any = [{ id: 5, SoQuyTrinh: 'PNK_0000_0000' }];
+  filter: any = {};
+  listdmKho: any = [];
+  trangThai: any = 1;
   paging: any = { CurrentPage: 1, TotalPage: 1, TotalItem: 100 };
   cols: any = [
     {
@@ -40,53 +40,53 @@ export class TonkhoComponent  extends StoreBase implements OnInit,OnDestroy {
       header: 'Số lượng',
       field: 'SoLuong',
       width: 'unset',
-      align:'center'
+      align: 'center'
 
     },
     {
       header: 'Trọng lượng',
       field: 'TongTrongLuong',
       width: 'unset',
-      align:'center'
+      align: 'center'
 
     },
   ];
-  mapLoaiKhoBong:any={
-    khobong:3,
-    khoxo:5,
-    khobonghoi:6,
-    khobongphe:7,
-    khohoiam:10,
-    khothanhpham:11,
+  mapLoaiKhoBong: any = {
+    khobong: 3,
+    khoxo: 5,
+    khobonghoi: 6,
+    khobongphe: 7,
+    khohoiam: 10,
+    khothanhpham: 11,
   }
-  checkQuyen:any={ChuaXuLy:true,DaXyLy:true,ThemMoi:true};
+  checkQuyen: any = { ChuaXuLy: true, DaXyLy: true, ThemMoi: true };
   listPhanXuong: any = [];
   listCaSanXuat: any = [];
-  loai : any = 0;
-  tenkho : any = "kho thành phẩm";
-  constructor(public _modal:NgbModal,public store:StoreService,public _toastr:ToastrService,private _service:SanXuatService,private activatedRoute: ActivatedRoute,private router:Router) {
+  loai: any = 0;
+  tenkho: any = "kho thành phẩm";
+  constructor(public _modal: NgbModal, public store: StoreService, public _toastr: ToastrService, private _service: SanXuatService, private activatedRoute: ActivatedRoute, private router: Router) {
     super(store)
   }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(res=>{
+    this.activatedRoute.params.subscribe(res => {
       console.log(this.mapLoaiKhoBong[`${res.kho}`])
       console.log(res);
       this.getListdmKho(this.mapLoaiKhoBong[`${res.kho}`]);
       this.loai = this.mapLoaiKhoBong[`${res.kho}`];
-      if(this.loai === 10)
+      if (this.loai === 10)
         this.tenkho = "kho hồi ẩm";
     })
     this.filter.KeyWord = '';
   }
   getListdmKho(Loai?) {
     let data = {
-      CurrentPage : 0,
-      Loai:Loai
+      CurrentPage: 0,
+      Loai: Loai
     }
     this._service.GetListdmKho(data).subscribe((res: any) => {
-      this.listdmKho = mapArrayForDropDown(res,'Ten','Id');
-      if(this.listdmKho.length > 0 && this.listdmKho !== undefined){
+      this.listdmKho = mapArrayForDropDown(res, 'Ten', 'Id');
+      if (this.listdmKho.length > 0 && this.listdmKho !== undefined) {
         this.filter.IddmKho = this.listdmKho[0].value;
         // this.listdmKho[0].select = true
         this.GetListQuyTrinh();
@@ -103,16 +103,16 @@ export class TonkhoComponent  extends StoreBase implements OnInit,OnDestroy {
   //     this.listPhanXuong = mapArrayForDropDown(res, 'Ten', 'Id');
   //   })
   // }
-  
-  changeTab(e){
-    this.trangThai = e.index+1;
+
+  changeTab(e) {
+    this.trangThai = e.index + 1;
     this.GetListQuyTrinh(true);
   }
-  changePage(event){
+  changePage(event) {
     this.paging.CurrentPage = event.page + 1;
     this.GetListQuyTrinh();
   }
-  GetListQuyTrinh(reset?){
+  GetListQuyTrinh(reset?) {
     // if(item.value !== undefined){
     //   this.listdmKho.forEach(element => {
     //     element.select = false;
@@ -130,15 +130,15 @@ export class TonkhoComponent  extends StoreBase implements OnInit,OnDestroy {
     }
     // this._service.getLuuKhoKhac(this.filter.IddmKho, '', this.paging.CurrentPage, this.filter.KeyWord).subscribe((res: any) => {
     this._service.GetLuuKhoTheKho(data).subscribe((res: any) => {
-      // this.items = res.items;
-      // this.paging = res.paging;
+      this.items = res.items;
+      this.paging = res.paging;
     })
   }
-  resetFilter(){
-    this.filter={
+  resetFilter() {
+    this.filter = {
       IddmKho: this.listdmKho[0].value,
-      Loai:this.loai,
-      KeyWord:''
+      Loai: this.loai,
+      KeyWord: ''
     };
     this.GetListQuyTrinh(true);
   }
@@ -152,9 +152,10 @@ export class TonkhoComponent  extends StoreBase implements OnInit,OnDestroy {
     modalRef.result.then((res: any) => {
       this.GetListQuyTrinh();
     })
-      .catch(er => { console.log(er) 
+      .catch(er => {
+        console.log(er)
         this.GetListQuyTrinh();
-    })
+      })
   }
   exportExcel(IddmItem) {
     let data: any = {
@@ -165,7 +166,7 @@ export class TonkhoComponent  extends StoreBase implements OnInit,OnDestroy {
       this._service.download(res.TenFile);
     })
   }
-  ngOnDestroy(){
+  ngOnDestroy() {
     super.ngOnDestroy();
   }
 }
